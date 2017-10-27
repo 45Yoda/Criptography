c73d1a624068966d292b4a5678c3b292a330513f
Guilherme Guerreiro <a73860@alunos.uminho.pt>
2017-10-27 14:54:20 +0100

    AES encryption with PBKDF2 key

--- Semana6/Cliente.py
+++ Semana6/Cliente.py
@@ -1,7 +1,12 @@
 # Código baseado em https://docs.python.org/3.6/library/asyncio-stream.html#tcp-echo-client-using-streams
 import asyncio
+import os
+import sys
 from Crypto.Cipher import AES
-from Crypto import Random
+from Crypto.Random import get_random_bytes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class Client:
     """ Classe que implementa a funcionalidade de um CLIENTE. """
@@ -27,7 +32,7 @@ def tcp_echo_client(loop=None):
     if loop is None:
         loop = asyncio.get_event_loop()
 
-    reader, writer = yield from asyncio.open_connection('127.0.0.1', 8888,
+    reader, writer = yield from asyncio.open_connection('127.0.0.1', 9999,
                                                         loop=loop)
 
     data = b'S'
@@ -36,11 +41,17 @@ def tcp_echo_client(loop=None):
     while len(data)>0:
         if msg:
             msg = b'M' + msg
-            iv = Random.new().read(AES.block_size)
-            key = hashlib.sha256(key.encode()).digest()
-            cypher = AES.new(key,AES.MODE.CBC,iv)
-            cypher_text = cypher.encrypt(msg)
-            writer.write(cypher_text)
+            # Key created with PBKDF2
+            key, salt = passwd()
+            # Encryption with AES block cipher
+            cipher = AES.new(key, AES.MODE_EAX)
+            nonce = cipher.nonce
+            # Encrypt and creates a tag for later verify if the message has not been corrupted
+            ciphertext, tag = cipher.encrypt_and_digest(msg)
+            # Nonce, tag and salt saved along with the encrypted message to send to the server
+            text = nonce + tag + salt + ciphertext
+            writer.write(text)
+            # End of encryption
             if msg[:1] == b'E': break
             data = yield from reader.read(100)
             if len(data)>0 :
@@ -57,6 +68,24 @@ def tcp_echo_client(loop=None):
 def run_client():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(tcp_echo_client())
+
+def passwd():
+    backend = default_backend()
+    salt = os.urandom(16)
+    # PBKDF2 derivation function
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=backend
+    )
+    # pass-phrase
+    password = b"Some password"
+    # Encrypt password with PBKDF2
+    key = kdf.derive(password)
+    # Return key and salt
+    return key, salt
 
 
 run_client()
--- Semana6/Servidor.py
+++ Semana6/Servidor.py
@@ -1,5 +1,11 @@
 # Código baseado em https://docs.python.org/3.6/library/asyncio-stream.html#tcp-echo-client-using-streams
 import asyncio
+import os
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 conn_cnt = 0
 
@@ -18,29 +24,43 @@ class ServerWorker(object):
 
 @asyncio.coroutine
 def handle_echo(reader, writer):
-    global conn_cnt
-    conn_cnt +=1
-    srvwrk = ServerWorker(conn_cnt)
-    data = yield from reader.read(100)
-    while True:
-        if data[:1]==b'E': break
-        if not data: continue
-        addr = writer.get_extra_info('peername')
-        res = srvwrk.respond(data[1:], addr)
-        if not res: break
-        res = b'M'+res
-        cyphertext = AES.new(key,AES.MODE_CBC)
-        text = cypher.decrypt(cyphertext)
-        writer.write(text)
-        yield from writer.drain()
-        data = yield from reader.read(100)
-    print("[%d]" % srvwrk.id)
-    writer.close()
+	global conn_cnt
+	conn_cnt +=1
+	srvwrk = ServerWorker(conn_cnt)
+	data = yield from reader.read(100)
+	while True:
+		if data[:1]==b'E': break
+		if not data: continue
+		# Get the data from the variable to start decryption
+		nonce = data[:16]
+		tag = data[16:32]
+		salt = data[32:48]
+		ciphertext = data[48:]
+		# Get key from PBKDF2
+		key = passwd(salt)
+		# AES cipher
+		cipher = AES.new(key,AES.MODE_EAX,nonce=nonce)
+		# Decryption
+		plaintext = cipher.decrypt(ciphertext)
+		try:
+			# Verify if the message has not been corrupted
+			cipher.verify(tag)
+			addr = writer.get_extra_info('peername')
+			res = srvwrk.respond(plaintext[1:], addr)
+			if not res: break
+			res = b'M'+res
+			writer.write(res)
+			yield from writer.drain()
+			data = yield from reader.read(100)
+		except ValueError:
+			continue
+	print("[%d]" % srvwrk.id)
+	writer.close()
 
 
 def run_server():
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+    coro = asyncio.start_server(handle_echo, '127.0.0.1', 9999, loop=loop)
     server = loop.run_until_complete(coro)
     # Serve requests until Ctrl+C is pressed
     print('Serving on {}'.format(server.sockets[0].getsockname()))
@@ -53,5 +73,22 @@ def run_server():
     loop.run_until_complete(server.wait_closed())
     loop.close()
     print('FINISHED!')
+
+def passwd(salt):
+    backend = default_backend()
+    # PBKDF2 derivation function
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=backend
+    )
+    # pass-phrase
+    password = b"Some password"
+    # Encrypt password with PBKDF2
+    key = kdf.derive(password)
+    # Return the secrets
+    return key
 
 run_server()
